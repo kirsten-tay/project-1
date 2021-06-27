07f7bb2bc262099948f3380e6242a7ba35b84451
kirsten tay <taykirsten11@gmail.com>
2021-06-27 21:05:23 +0000

    updated icons

--- src/components/Sec.jsx
+++ src/components/Sec.jsx
@@ -1,32 +1,40 @@
  import react, { Fragment } from 'react';
 import image from './../Assets/image.jpg';
-import photoSvg from './photoSvg';
+import PaymentSvg from './PaymentSvg';
+
  
 const Sec=(Sec)=>{
     return(
         <Fragment>
         <div className="flex flex-row">
             <div className="flex flex-1 justify-begin items-center">
-                        <img src={image} className="h-full"/>
+                        <img src={image} />
                     </div>
 
-             <div className="flex-1 px-20 py-16">
-             <div className="flex justify-around  flex-col items-center">
-                    <div className="flex-row" >
-                      <div className=" flex flex-col h-32 justify-between">  <photoSvg /></div>
-                        <span className="font-bold text-xl">Create account</span>
+             <div className="flex flex-col flex justify-around   py-3 px-40  items-center">
+                    <div className=" flex flex-row" >
+                      <div className=" flex flex-col justify-begin px-5">  <PaymentSvg /></div>
+                      <div className=" flex flex-col justify-begin px-5"> 
+                        <span className="font-bold text-xl">PAYMENTS</span>
+                        <span>Lorem ipsum dolor sit amet consectetur.</span></div>
+
                     </div>
                    
-                    <div className="flex row">
-                        <photoSvg />
+                    <div className="flex flex-row">
+                    <div className=" flex flex-col justify-begin px-5"> <PaymentSvg /> </div>
+                    <div className=" flex flex-col justify-begin px-5"> 
                         <span className="font-bold text-xl">Link your bank account</span>
+                        <span>Lorem ipsum dolor sit amet consectetur </span> </div>
                     </div>
-                    <div >
-                        <imgSvg />
+                    <div className="flex flex-row" >
+                    <div className=" flex flex-col justify-begin px-5"> <PaymentSvg /> </div>
+                    <div className=" flex flex-col justify-begin px-5"> 
                         <span className="font-bold text-xl">Start buying & selling</span>
+                        <span>Lorem ipsum dolor sit amet consectetuer.</span>
+                            </div>
                     </div>
                 </div>
-             </div>
+             
             
         
         </div>
